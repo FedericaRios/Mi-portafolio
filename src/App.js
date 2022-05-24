90e4be5a2dee721c6d4d0ba557783ed857b0b015
FedericaRios <rojofederica21@gmail.com>
2022-05-24 20:36:05 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Presentation from './pages/presentation';
 import Home from './pages/home';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Presentation />} />
         <Route exact path="/home" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
